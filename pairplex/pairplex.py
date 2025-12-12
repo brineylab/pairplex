850869149168223efc0739fb7492037e8a7439c1
bnemoz <bnemoz@gmail.com>
2025-12-11 16:08:26 -0800

    Add failsafe for empty well annotation step

--- pairplex/pairplex.py
+++ pairplex/pairplex.py
@@ -388,6 +388,9 @@ def run(
             # --------------------
 
             main_pbar.set_postfix_str("annotating sequences", refresh=True)
+            # failsafe for empty consensus files
+            if consensus_count == 0:
+                continue
             # guard against MMSeqs threading issues with small datasets
             consensus_count = filtered_df.shape[0]
             mmseqs_threads = None
